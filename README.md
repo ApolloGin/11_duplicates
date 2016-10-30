6c0653bd086fade4d0f8e312088837626d0adcd9
Toxagin <toxagin@gmail.com>
2016-10-30 21:27:54 +0300

    created two functions in module duplicates.py, documented it in README.md

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # 11_duplicates
+
+## Synopsis
+
+Script helps detect duplicates of file in specified directory.
+
+> Note: File are duplicates, if both files have same name and size.
+
+## Quick start
+
+ - Run duplicates.py 'python3.5 duplicates.py'
+ - **Input**: Directory path, which you want to scan
+ - **Output**: List of files which has duplicates and all full paths
+
+## Module functions
+
+ - `find_duplicates(root_directory)` - finds duplicates in directory and all sudirectories in specified path. Return `dict` value. Keys of the folowing dictionary is file names and values is `list` of full paths to the all duplicate files.
+ - `show_duplicates(duplicates_dict)` - pretty print the result of the `find_duplicates(root_directory)` function.
--- duplicates.py
+++ duplicates.py
@@ -1,8 +1,21 @@
+from collections import defaultdict
+import os
+
+def find_duplicates(root_directory):
+    dup_files = defaultdict(list)
+    for dirpath, dirnames, files in os.walk(root_directory):
+        for filename in files:
+            full_file_path = os.path.join(dirpath, filename)
+            file_size = os.stat(full_file_path).st_size
+            dup_files[(filename, file_size)].append(full_file_path)
+    return {key[0]: val for key, val in dup_files.items() if len(val) > 1}
 
 
-def are_files_duplicates(file_path1, file_path_2):
-    pass
-
+def show_duplicates(duplicates_dict):
+    for key, value in duplicates_dict.items():
+        print('File "{0}" found in following directories:'.format(key))
+        for v in value:
+            print('    {0}'.format(v))
 
 if __name__ == '__main__':
-    pass
+    show_duplicates(find_duplicates(input('Enter root directory: ')))
